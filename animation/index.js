46e6314e21988c914e662d0ede2ff556c39499f1
binchu <leoalegutierrez97@gmail.com>
2021-11-12 16:50:31 -0300

    estilos en la sección contacto y empezamos con animaciones para el sitio

---
+++ animation/index.js
@@ -0,0 +1,14 @@
+// animación show-hide navbar
+let oldScrollPosition = window.pageYOffset;
+
+const showHideNav = () => {
+    let newScrollPosition = window.pageYOffset;
+    if(oldScrollPosition < newScrollPosition){
+        navBar.classList.add("hideNav");
+    } else {
+        navBar.classList.remove("hideNav");
+    }
+    oldScrollPosition = newScrollPosition;
+}
+
+window.onscroll = showHideNav;
