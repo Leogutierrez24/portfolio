6af5218a33282b3bb8e2d115aa938135679b8a41
binchu <leoalegutierrez97@gmail.com>
2021-11-16 16:45:38 -0300

    cambio completo en el fade de los proyectos. Solo css ahora

--- animation/dom.js
+++ animation/dom.js
@@ -1,15 +1,3 @@
 // header
 const navBar = document.getElementById("navBar");
 
-// proyectos
-const firstProject = document.getElementById("first-project");
-const firstText = document.getElementById("first-project__text");
-const firstLink = document.getElementById("first-project__link");
-
-const secondProject = document.getElementById("second-project");
-const secondText = document.getElementById("second-project__text");
-const secondLink = document.getElementById("second-project__link");
-
-const thirdProject = document.getElementById("third-project");
-const thirdText = document.getElementById("third-project__text");
-const thirdLink = document.getElementById("third-project__link");
--- animation/index.js
+++ animation/index.js
@@ -13,32 +13,4 @@ const showHideNav = () => {
 
 window.onscroll = showHideNav;
 
-// animación show-hide texto en el item proyecto
-
-firstProject.onmouseover = (() => {
-    firstText.classList.add("hide");
-    firstLink.classList.add("show");
-});
-firstProject.onmouseleave = (() => {
-    firstText.classList.remove("hide");
-    firstLink.classList.remove("show");
-});
-
-secondProject.onmouseover = (() => {
-    secondText.classList.add("hide");
-    secondLink.classList.add("show");
-});
-secondProject.onmouseleave = (() => {
-    secondText.classList.remove("hide");
-    secondLink.classList.remove("show");
-});
-
-thirdProject.onmouseover = (() => {
-    thirdText.classList.add("hide");
-    thirdLink.classList.add("show");
-});
-thirdProject.onmouseleave = (() => {
-    thirdText.classList.remove("hide");
-    thirdLink.classList.remove("show");
-});
 
